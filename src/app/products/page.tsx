6d8aea8711cc115d22cc4814fa2eaff381592ca9
Freek.Sartrys <66180171+FreekSa@users.noreply.github.com>
2025-05-17 22:30:22 +0200

    trying button click version 1

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,18 +1,34 @@
-
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 export default async function Page() {
   const cookieStore = cookies();
-  const supabase = createClient(cookieStore)
+  const supabase = createClient(cookieStore);
 
-  const { data: products } = await supabase.from('Product').select('*')
-  console.log('todos: ' + products);
+  const { data: products } = await supabase.from("Product").select("*");
+  console.log("products:", products);
+
   return (
-    <ul>
-      {products?.map((p) => (
-        <li>{p.name}</li>
-      ))}
-    </ul>
-  )
+    <div>
+      <ul>
+        {products?.map((p) => (
+          <li key={p.id}>{p.name}</li>
+        ))}
+      </ul>
+      <button onClick={() => AddProduct(supabase)}>Add</button>
+    </div>
+  );
+  async function AddProduct(supabase: any) {
+    const { data, error } = await supabase
+      .from("Product")
+      .insert([{ name: "Coca Cola" }]);
+  
+    if (error) {
+      console.error("Error inserting product:", error);
+    } else {
+      console.log("Inserted product:", data);
+    }
+    return;
+  }
 }
+
